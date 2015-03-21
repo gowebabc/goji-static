e909ea0c39bc08f8ea8beb3ea9da81f5297398d0
Patrick D'appollonio <patrick@dappollonio.us>
2015-03-21 19:28:49 -0400

    Enables ETag Support.

--- static.go
+++ static.go
@@ -1,6 +1,7 @@
 package static
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -21,7 +22,18 @@ func Static(directory string) func(c *web.C, h http.Handler) http.Handler {
 					if path, err := filepath.Abs(originalPath); err == nil {
 						if fi, err := os.Stat(path); err == nil && path != "" {
 							if !fi.IsDir() {
-								w.Header().Add("Cache-Control", "public, max-age=31536000")
+								hour, minute, second := fi.ModTime().Clock()
+								etag := fmt.Sprintf(`"%d%d%d%d%d"`, fi.Size(), fi.ModTime().Day(), hour, minute, second)
+								w.Header().Set("Cache-Control", "public, max-age=31536000")
+								w.Header().Set("Etag", etag)
+
+								if match := r.Header.Get("If-None-Match"); match != "" {
+									if strings.Contains(match, etag) {
+										w.WriteHeader(http.StatusNotModified)
+										return
+									}
+								}
+
 								http.ServeFile(w, r, path)
 								return
 							}
@@ -33,6 +45,7 @@ func Static(directory string) func(c *web.C, h http.Handler) http.Handler {
 			h.ServeHTTP(w, r)
 			return
 		}
+
 		return http.HandlerFunc(fn)
 	}
 }
